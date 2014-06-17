d3013bbbf5023c3460453c3a46f5ebb55c2257b2
Krzysztof Rygielski <krzysztof@rygiel.net>
2014-06-18 00:41:42 +0200

    unit tests

--- test/models/time_entry_test.rb
+++ test/models/time_entry_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe TimeEntry do
-  it "must be valid" do
-    time_entry.must_be :valid?
+  setup do
+    activate_authlogic
+    UserSession.create users(:user)
+  end
+
+  it 'generates weekly report' do
+    result = TimeEntry.weekly_report users(:user).id
+
+    avg_distance = (time_entries(:two).distance + time_entries(:three).distance) / 2
+    avg_speed = (time_entries(:two).average_speed + time_entries(:three).average_speed) / 2
+
+    assert_equal 2, result.size
+    assert_equal avg_distance, result[1]['avg_distance']
+    assert_equal avg_speed, result[1]['avg_speed']
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new(email: 'user@email.com', password: 'somepassword', password_confirmation: 'somepassword') }
 
   it "must be valid" do
     user.must_be :valid?
