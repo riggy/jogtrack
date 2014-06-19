be6b4247a7dcc9ba6ac3e426d920454ee64e659a
Krzysztof Rygielski <krzysztof@rygiel.net>
2014-06-19 22:32:07 +0200

    changed scopes

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,11 +2,11 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :user
 
   scope :date_from, ->(from_date) do
-    where("date > ?", from_date) unless from_date.blank?
+    where("date >= ?", from_date) unless from_date.blank?
   end
 
   scope :date_to, ->(to_date) do
-    where("date < ?", to_date) unless to_date.blank?
+    where("date <= ?", to_date) unless to_date.blank?
   end
 
   def self.weekly_report(user_id)
